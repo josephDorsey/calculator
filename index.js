4be0f5a8671e0f96d548e4d35cf86c27cd1b92e6
josephDorsey <jmdorsey92@gmail.com>
2023-07-12 23:57:20 -0700

    init commit 🚀

---
+++ index.js
@@ -0,0 +1,46 @@
+// Functions
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const sum = function (arr) {
+  let sum = arr.reduce((acc, cur) => acc + cur, 0);
+  return sum;
+};
+
+const multiply = function (...args) {
+  let sum = args.reduce((acc, cur) => acc * cur, 1);
+  return sum;
+};
+
+const power = function (a, b) {
+  return a ** b;
+};
+
+const factorial = function (num) {
+  let sum = 1;
+  for (let i = 1; i <= num; i++) {
+    sum *= i;
+  }
+  return sum;
+};
+
+const clear = document.querySelector(".clear");
+const number = document.querySelector(".number");
+const total = document.querySelector(".total");
+const bottomButtons = document.querySelector(".bottom-buttons");
+const topButtons = document.querySelector(".top-buttons");
+topButtons.addEventListener("click", (e) => {
+  if (e.target.name === "clear") {
+    number.textContent = "";
+    total.textContent = "";
+  }
+});
+
+bottomButtons.addEventListener("click", (e) => {
+  console.log(e.target);
+});
