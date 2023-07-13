f5fab24e2946a3c42641a3de0207543d8fc2237d
josephDorsey <jmdorsey92@gmail.com>
2023-07-13 12:50:40 -0700

    feat: Implement adding operator

--- index.js
+++ index.js
@@ -30,74 +30,72 @@ const factorial = function (num) {
 };
 
 const clear = document.querySelector(".clear");
-const number = document.querySelector(".number");
 const total = document.querySelector(".total");
+const equation = document.querySelector(".equation");
 const bottomButtons = document.querySelector(".bottom-buttons");
 const topButtons = document.querySelector(".top-buttons");
-const state = ["add", "divide", "multiply", "equals", "subtracts"];
+// const state = ["typing", "add", "divide", "multiply", "equals", "subtracts"];
+const operators = ["+", "-", "x", "/", "="];
 const numberEntry = [];
 let mode = null;
 topButtons.addEventListener("click", (e) => {
   if (e.target.name === "clear") {
-    number.textContent = "";
-    total.textContent = "";
+    total.value = "";
+    equation.textContent = "";
+    state = "typing";
   }
 });
 
-bottomButtons.addEventListener("click", (e) => {
-  if (state[mode] === "add") {
-    number.textContent = "";
-    total.textContent += +e.target.value;
-    numberEntry.push(+e.target.value);
-    console.log(numberEntry);
-    mode = null;
-  }
-  if (e.target.value === "7") {
-    number.textContent += "7";
-  }
-  if (e.target.value === "8") {
-    number.textContent += "8";
-  }
-  if (e.target.value === "9") {
-    number.textContent += "9";
-  }
-  if (e.target.value === "6") {
-    number.textContent += "6";
-  }
-  if (e.target.value === "5") {
-    number.textContent += "5";
-  }
-  if (e.target.value === "4") {
-    number.textContent += "4";
-  }
-  if (e.target.value === "3") {
-    number.textContent += "3";
-  }
-  if (e.target.value === "2") {
-    number.textContent += "2";
-  }
-  if (e.target.value === "1") {
-    number.textContent += "1";
-  }
-  if (e.target.value === "0") {
-    number.textContent += "0";
-  }
+const calculator = {
+  state: "typing",
+  total: "",
+  equation: "",
+  sumAll: [],
+  operators: ["+", "-", "x", "/", "="],
+};
 
-  if (e.target.value === "+") {
-    if (total.textContent.includes("=")) {
-      const [, , sum] = total.textContent.split("=");
-      total.textContent = sum;
+function addOperator(num, arr) {
+  arr.push(num);
+  return arr.reduce((acc, cur) => acc + cur);
+}
+bottomButtons.addEventListener("click", (e) => {
+  if (
+    e.target.value !== "+" &&
+    e.target.value !== "x" &&
+    e.target.value !== "/" &&
+    e.target.value !== "-" &&
+    e.target.value !== "="
+  ) {
+    if (equation.textContent.includes("+") && calculator.state === "add") {
+      total.value = "";
+      calculator.state = "typing";
     }
-    mode = 0;
-    numberEntry.push(+number.textContent);
-    total.textContent += `${number.textContent}+`;
-    console.log(numberEntry);
+    if (equation.textContent.includes("+") && calculator.state === "end") {
+      total.value = "";
+      equation.textContent = "";
+      calculator.state = "typing";
+    }
+    if (equation.textContent.includes("+") && calculator.state === "add") {
+      total.value = "";
+      calculator.state = "typing";
+    }
+    if (calculator.state === "typing") {
+      calculator.total = total.value += e.target.value;
+    }
   }
-  if (e.target.value === "equal") {
-    let sum = numberEntry.reduce((acc, cur) => acc + cur, 0);
-    total.textContent += `=${sum}`;
-    number.textContent = sum;
-    numberEntry.length = 0;
-    console.log(numberEntry);
+  if (e.target.value === "+") {
+    calculator.state = "add";
+    if (equation.textContent.includes("+")) {
+      total.value = addOperator(+total.value, calculator.sumAll);
+    } else {
+      calculator.sumAll.push(+total.value);
+    }
+    equation.textContent = `${total.value}+`;
+  }
+  if (e.target.value === "=") {
+    calculator.state = "end";
+    equation.textContent += `${total.value}=`;
+    total.value = addOperator(+total.value, calculator.sumAll);
+    calculator.sumAll.length = 0;
   }
 });
